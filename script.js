ba63d7f182c9d68b0442e05aa9e72146b5f34184
Randy Murrow <randallmurrow@gmail.com>
2025-08-12 08:41:56 -0500

    Add index.html and script.js with initial pseudocode for single round of game

---
+++ script.js
@@ -0,0 +1,11 @@
+// GET random choice of "rock," "paper," or "scissors"
+
+// GET user input via prompt
+
+// Store user and computer scores
+
+// Play a single round
+    // Define parameters for human and computer choices
+    // Make choices case-insensitive
+    // Log result of game to console
+    // Increment winner's score
